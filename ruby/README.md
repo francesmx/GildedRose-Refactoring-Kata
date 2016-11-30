bf330bd86b1863330ed9ede684630bfc8ad874dc
Frances Maxwell <fmaxwell@gmail.com>
2016-11-30 14:19:48 +0000

    Added in further tests for update_quality

--- ruby/README.md
+++ ruby/README.md
@@ -8,9 +8,9 @@ Hi and welcome to team Gilded Rose. As you know, we are a small inn with a prime
 
 ## The Rules
 * Once the sell by date has passed, Quality degrades twice as fast
-* The Quality of an item is never negative
-* “Aged Brie” actually increases in Quality the older it gets
-* The Quality of an item is never more than 50
+* The Quality of an item is never negative - DONE
+* “Aged Brie” actually increases in Quality the older it gets - DONE
+* The Quality of an item is never more than 50 - DONE
 * “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
 * “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
       Quality increases     by 2 when there are 10 days or less
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -4,6 +4,32 @@ require_relative 'spec_helper'
 
 describe GildedRose do
 
+  describe "#update_quality" do
+
+    before(:each) do
+      items = [Item.new("MyItem", 10, 40)]
+      @gilded_rose = GildedRose.new(items)
+    end
+
+    it 'reduces the quality by 1 each time' do
+      @gilded_rose.update_quality
+      expect(@gilded_rose.items[0].quality).to eq 39
+    end
+
+    it "will not reduce the quality beyond the minimum" do
+      41.times do
+        @gilded_rose.update_quality
+      end
+      expect(@gilded_rose.items[0].quality).to eq GildedRose::MIN_QUALITY
+    end
+
+    it 'reduces the sell_in by 1 each time' do
+      @gilded_rose.update_quality
+      expect(@gilded_rose.items[0].sell_in).to eq 9
+    end
+
+  end
+
   describe "#update_quality for Aged Brie" do
 
     before(:each) do
@@ -14,6 +40,13 @@ describe GildedRose do
     it "increases in quality the older it gets" do
       @gilded_rose.update_quality
       expect(@gilded_rose.items[0].quality).to eq 41
+    end
+
+    it "will not increase the quality beyond the maximum" do
+      GildedRose::MAX_QUALITY.times do
+        @gilded_rose.update_quality
+      end
+      expect(@gilded_rose.items[0].quality).to eq GildedRose::MAX_QUALITY
     end
 
   end
